Fixed quaternion spline interpolation

soulstruct_havok/spline_compression.py
@@ -23,6 +23,7 @@ import struct
 import typing as tp
 from enum import IntEnum
 
+import numpy as np
 from soulstruct.utilities.binary import *
 
 from soulstruct_havok.utilities.maths import Vector3, Vector4, Quaternion, TRSTransform
@@ -166,9 +167,28 @@ def find_knot_span(degree: int, value: float, control_point_count: int, knots: l
     return guess  # found knot index
 
 
-def get_single_point(
-    knot_span_index: int, degree: int, frame: float, knots: list[int], control_points: list[tp.Union[float, Quaternion]]
-) -> tp.Union[float, Quaternion]:
+def get_single_point_float(
+    knot_span_index: int, degree: int, frame: float, knots: list[int], control_points: list[float]
+) -> float:
+    """Basis_ITS1, GetPoint_NR1, TIME-EFFICIENT NURBS CURVE EVALUATION ALGORITHMS, pages 64 & 65"""
+    n = [1.0, 0.0, 0.0, 0.0, 0.0]
+
+    for i in range(1, degree + 1):
+        for j in range(i - 1, -1, -1):
+            a = (frame - knots[knot_span_index - j]) / (knots[knot_span_index + i - j] - knots[knot_span_index - j])
+            tmp = n[j] * a
+            n[j + 1] += n[j] - tmp
+            n[j] = tmp
+
+    value = 0.0
+    for i in range(0, degree + 1):
+        value += control_points[knot_span_index - i] * n[i]
+    return value
+
+
+def get_single_point_quaternion(
+    knot_span_index: int, degree: int, frame: float, knots: list[int], control_points: list[Quaternion]
+) -> Quaternion:
     """Basis_ITS1, GetPoint_NR1, TIME-EFFICIENT NURBS CURVE EVALUATION ALGORITHMS, pages 64 & 65
 
     Works for either `float` or `Quaternion` control points.
@@ -182,10 +202,11 @@ def get_single_point(
             n[j + 1] += n[j] - tmp
             n[j] = tmp
 
-    value = Quaternion.identity() if isinstance(control_points[0], Quaternion) else 0.0
+    # NOTE: `Quaternion` cannot be used as the accumulator here because it cannot hold zero-norm quaternions.
+    value = np.zeros(4, dtype=np.float32)
     for i in range(0, degree + 1):
-        value += control_points[knot_span_index - i] * n[i]
-    return value
+        value += control_points[knot_span_index - i].data * n[i]
+    return Quaternion(value)
 
 
 class SplineHeader:
@@ -306,7 +327,9 @@ class TrackVector3:
         axis_value = getattr(self, axis)
         if isinstance(axis_value, SplineFloat):
             knot_span = find_knot_span(self.spline_header.degree, frame, len(axis_value), self.spline_header.knots)
-            return get_single_point(knot_span, self.spline_header.degree, frame, self.spline_header.knots, axis_value)
+            return get_single_point_float(
+                knot_span, self.spline_header.degree, frame, self.spline_header.knots, axis_value
+            )
             # return get_single_point_new(self.spline_header.degree, frame, self.spline_header.knots, axis_value)
         else:
             return axis_value  # float
@@ -518,7 +541,9 @@ class TrackQuaternion:
     def get_quaternion_at_frame(self, frame: float) -> Quaternion:
         if isinstance(self.value, SplineQuaternion):
             knot_span = find_knot_span(self.spline_header.degree, frame, len(self.value), self.spline_header.knots)
-            return get_single_point(knot_span, self.spline_header.degree, frame, self.spline_header.knots, self.value)
+            return get_single_point_quaternion(
+                knot_span, self.spline_header.degree, frame, self.spline_header.knots, self.value
+            )
         else:
             return self.value  # Quaternion
 

soulstruct_havok/utilities/maths/quaternion.py
@@ -36,14 +36,14 @@ class Quaternion:
     _IDENTITY: tp.ClassVar[Rotation] = Rotation.identity()
 
     rotation: Rotation = field(default_factory=Rotation.identity)
-    _data: tuple[float, float, float, float] = None
+    _data: np.ndarray = None
 
     def __init__(self, xyzw: np.ndarray | list | tuple | Vector4 | Quaternion | Rotation):
         if isinstance(xyzw, Rotation):
             object.__setattr__(self, "rotation", xyzw)
             object.__setattr__(self, "_data", None)  # will be generated on first access
         else:
-            object.__setattr__(self, "_data", tuple(xyzw))
+            object.__setattr__(self, "_data", np.array(xyzw))  # could be zero-norm
             try:
                 object.__setattr__(self, "rotation", Rotation.from_quat(xyzw))
             except ValueError:
@@ -59,7 +59,7 @@ class Quaternion:
     def data(self):
         """Returns the quaternion as a tuple of 4 floats. Cached on first access."""
         if self._data is None:
-            object.__setattr__(self, "_data", tuple(self.rotation.as_quat()))
+            object.__setattr__(self, "_data", self.rotation.as_quat())
         return self._data
 
     def get_real(self) -> float:
@@ -167,9 +167,12 @@ class Quaternion:
         """Shorter wrapper for the above."""
         return cls.from_axis_angle(Vector3(xyz), angle, radians)
 
-    def to_euler_angles(self, radians=False) -> Vector3:
-        mat = Matrix3(self.to_matrix3())
-        return mat.to_euler_angles(radians=radians)
+    def to_euler_angles(self, radians=False, order="xzy") -> Vector3:
+        """Decompose Quaternion (via Matrix3 representation) into Euler angles.
+
+        NOTE: Can only decompose in 'xzy' order right now and will raise an error if the order is not 'xzy'.
+        """
+        return self.to_matrix3().to_euler_angles(radians=radians, order=order)
 
     # endregion
 
@@ -325,6 +328,11 @@ class Quaternion:
 
     @staticmethod
     def slerp(q1: Quaternion, q2: Quaternion, t: float) -> Quaternion:
-        """Spherically interpolate between two Quaternions by parameter `t` in interval [0, 1]."""
-        scipy_slerp = Slerp([0, 1], Rotation.concatenate([q1.rotation, q2.rotation]))
+        """Spherically interpolate between two Quaternions by parameter `t` in interval [0, 1].
+
+        NOTE: As `Rotation` cannot hold zero-norm quaternions -- but we sometimes need to slerp with these -- the
+        `data` representation is used.
+        """
+        # print(q1.data, q2.data)
+        scipy_slerp = Slerp([0, 1], Rotation.concatenate([Rotation.from_quat(q1.data), Rotation.from_quat(q2.data)]))
         return Quaternion(scipy_slerp(t))